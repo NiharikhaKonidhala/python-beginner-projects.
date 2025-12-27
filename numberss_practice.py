97174c900f2e505df859cd1c856827be1fcb6060
NiharikhaKonidhala <klpathi2030@gmail.com>
2025-12-27 19:41:58 +0400

    Refactor variable names for consistency

--- numberss_practice.py
+++ numberss_practice.py
@@ -1,13 +1,15 @@
+
 Math = 95
 Science = 85
 English = 90
-Social Studies = 87
+SST = 87
 Arabic = 89
-Hindi/Second Language = 90
+Second_Language = 90
 
 
-total = Math + Science + English + Social Studies + Arabic + Hindi/Second Language
+total = Math+Science+English+SST+Arabic+Second_Language
 percentage = total / 6
 
 print("Total marks:", total)
 print("Percentage:", percentage)
+
