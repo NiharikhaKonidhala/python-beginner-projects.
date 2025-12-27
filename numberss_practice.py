1dfbb0c2bf5fdbf0315f09bb3744d246ec074c7c
NiharikhaKonidhala <klpathi2030@gmail.com>
2025-12-27 19:36:07 +0400

    Add files via upload

---
+++ numberss_practice.py
@@ -0,0 +1,13 @@
+Math = 95
+Science = 85
+English = 90
+Social Studies = 87
+Arabic = 89
+Hindi/Second Language = 90
+
+
+total = Math + Science + English + Social Studies + Arabic + Hindi/Second Language
+percentage = total / 6
+
+print("Total marks:", total)
+print("Percentage:", percentage)
